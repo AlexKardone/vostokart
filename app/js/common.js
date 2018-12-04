a8b81a70a4e0ab37eada03b2f7b7c90e301e39ca
Alexander Matskevich <alexandermatskevich@RD-Panasenko.local>
2018-12-04 18:04:29 +0300

    Work on adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -32,31 +32,41 @@ $(document).ready(function() {
 
 
 
-  // $('.page-header__navbar--bottom a').on('click', function(e){
-  //   e.preventDefault();
-  // });
+  $('.page-header__navbar--bottom a').on('click', function(e){
+    e.preventDefault();
+  });
      
-  // $('#menuFurniture').hover(function () {
-  //    clearTimeout($.data(this,'timer'));
-  //    $('#dropMenu',this).stop(true,true).slideDown(200);
-  // }, function () {
-  //   $.data(this,'timer', setTimeout($.proxy(function() {
-  //     $('#dropMenu',this).stop(true,true).slideUp(200);
-  //   }, this), 100));
-  // });
+  $('.page-header__bottom-menu-list li').hover(function () {
+     clearTimeout($.data(this,'timer'));
+     $('.dropdown-menu',this).stop(true,true).fadeIn(200);
+  }, function () {
+    $.data(this,'timer', setTimeout($.proxy(function() {
+      $('.dropdown-menu',this).stop(true,true).fadeOut(500);
+    }, this), 100));
+  });
  
 
 
   $("#carouselHero").owlCarousel({
   	items: 1,
   	nav: true,
-  	// navText: ['<i class="fas fa-angle-left fa-10x"></i>', '<i class="fas fa-angle-right fa-10x"></i>'],
   	dots: true,
   	loop: true
   });
 
   $("#carouselList").owlCarousel({
   	items: 4,
+    responsive:{
+      0: {
+        items: 1
+      },
+      768: {
+        items: 3
+      },
+      769: {
+        items: 4
+      }
+    },
   	margin: 10,
   	nav: true,
   	// navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
